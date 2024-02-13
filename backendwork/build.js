cadc626cf21b82af2db6c5d5d0f04b4a3b62c82c
Nicholas <aigberuan6@gmail.com>
2024-02-13 09:35:59 +0100

    restruct

--- backendwork/build.js
+++ backendwork/build.js
@@ -3,11 +3,11 @@ const less = require('less');
 const { minify } = require('html-minifier');
 const { minify: minifyJS } = require('uglify-js');
 const concat = require('concat');
+const fs = require('fs');
 
 // Compile SASS to CSS
 sass.render({ file: 'styles.scss' }, (err, result) => {
     if (!err) {
-        // Save compiled CSS to a file
         fs.writeFileSync('styles.css', result.css);
     }
 });
@@ -15,7 +15,6 @@ sass.render({ file: 'styles.scss' }, (err, result) => {
 // Compile LESS to CSS
 less.render(fs.readFileSync('styles.less', 'utf8'), (err, result) => {
     if (!err) {
-        // Save compiled CSS to a file
         fs.writeFileSync('styles.css', result.css);
     }
 });
@@ -34,6 +33,4 @@ fs.writeFileSync('index.min.html', minifiedHTML);
 // Merge HTML parts into JS
 const headerHTML = fs.readFileSync('header.html', 'utf8');
 const footerHTML = fs.readFileSync('footer.html', 'utf8');
-concat(['header.js', '-n', 'footer.js'], 'output.js', () => {
-    fs.writeFileSync('output.js', headerHTML + footerHTML);
-});
+fs.writeFileSync('output.js', headerHTML + footerHTML);
